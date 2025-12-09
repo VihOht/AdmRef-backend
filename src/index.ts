3bd5120ecebef9cff595f4979ec98ff9e019f459
VihOht <robsonsf@al.insper.edu.br>
2025-12-09 14:29:37 -0500

    feat: adding cors

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@
 import express from 'express';
 import { mainRouter } from './routes'
 import middlewares from './middlewares';
+import cors from 'cors';
 
 const app = express();
 
 // Apply middlewares
 app.use(express.json());
 app.use(middlewares.jwtValidator);
+app.use(cors());
 
 app.use('/api', mainRouter);
 
